97eb316beb745552a37c906506968a92a004a1f4
FXPipPioneers <fxpippioneers@gmail.com>
2025-06-19 20:25:32 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    token: process.env.DISCORD_TOKEN || 'MTM4NTI5Mzc2OTgyOTUxNTMyNA.GhaJIl.Sjo8Kpgrf6qG9BxFxYILq7emWkTQH4tJtfNkkM',
-    clientId: process.env.DISCORD_CLIENT_ID || '1385293769829515324'
-};
