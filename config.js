fbba5d21fe89daa4eecd77727ba54afd44e44e03
FXPipPioneers <fxpippioneers@gmail.com>
2025-06-19 20:16:37 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    token: process.env.DISCORD_TOKEN || 'your_discord_bot_token_here',
-    clientId: process.env.DISCORD_CLIENT_ID || 'your_discord_client_id_here'
-};
