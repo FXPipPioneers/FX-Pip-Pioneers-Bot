dd5ca18e88093f11b60683caa73daf06fd62e194
FXPipPioneers <fxpippioneers@gmail.com>
2025-06-19 20:18:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.DISCORD_TOKEN || 'your_discord_bot_token_here',
-    clientId: process.env.DISCORD_CLIENT_ID || 'your_discord_client_id_here'
+    token: process.env.DISCORD_TOKEN || 'MTM4NTI5Mzc2OTgyOTUxNTMyNA.GhaJIl.Sjo8Kpgrf6qG9BxFxYILq7emWkTQH4tJtfNkkM',
+    clientId: process.env.DISCORD_CLIENT_ID || '1385293769829515324'
 };
